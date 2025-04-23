c712510dd68c708c95416a2176265792d3bfbb6a
rivafanap <cursoadsfanap@gmail.com>
2025-04-23 15:56:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const sectionId = link.getAttribute('href').substring(1);
+            const section = document.getElementById(sectionId);
+            section.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
 
